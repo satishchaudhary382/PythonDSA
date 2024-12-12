9eae9fb1e85ad9a74353bd5e8cc08912f6ba1bdd
satishchaudhary382 <chaudharysatish382@gmail.com>
2024-12-12 22:04:39 +0545

    Adding the cubes as well

--- R-1-4.py
+++ R-1-4.py
@@ -10,3 +10,8 @@ print(sum_of_squares(343.3))
 print(sum_of_squares(3423.3))
 print(sum_of_squares(3413.3))
 print(sum_of_squares(3433.3))
+
+def cube_of_number(n:float) -> float:
+	return n*n*n
+
+print(cube_of_number(23))
