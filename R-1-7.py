d0c7d9ce63e10d232d84190b19e182d1634f3cd7
satishchaudhary382 <chaudharysatish382@gmail.com>
2024-12-13 07:33:32 +0545

    using list comprehension

--- R-1-7.py
+++ R-1-7.py
@@ -1,3 +1,13 @@
 # Give a single command that computes the sum from Exercise R-1.6, 
 # relying on Python’s comprehension syntax and the built-in sum function.
 
+def sum_of_odd(n):
+	index = 0
+	s = []
+	while index < n:
+		if index % 2 != 0:
+			# s += index
+			s.append(index)
+		index += 1
+	print(s, sum(s))
+sum_of_odd(6)
